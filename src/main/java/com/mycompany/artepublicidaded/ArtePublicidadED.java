5ba9e41953edcf884b617a7b3ff77bf4244c026b
glendaeh3-maker <glendaeh3@gmail.com>
2026-06-22 19:52:27 -0500

    configuramos el main para que ejecute primero el forms de login :D

--- src/main/java/com/mycompany/artepublicidaded/ArtePublicidadED.java
+++ src/main/java/com/mycompany/artepublicidaded/ArtePublicidadED.java
@@ -10,5 +10,10 @@ package com.mycompany.artepublicidaded;
 public class ArtePublicidadED {
 
     public static void main(String[] args) {
-    }
+         java.awt.EventQueue.invokeLater(new Runnable() {
+        public void run() {
+            new LoginForm().setVisible(true);
+        }
+    });
+  }
 }
